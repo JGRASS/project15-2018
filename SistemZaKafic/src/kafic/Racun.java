d4891331e1965b5aad225cd5309a705b9a85aba6
nikolab20 <nikola.bakic20@hotmail.com>
2018-04-30 14:26:47 +0200

    Implementirana i dokumentovana klasa Racun

--- SistemZaKafic/src/kafic/Racun.java
+++ SistemZaKafic/src/kafic/Racun.java
@@ -1,5 +1,300 @@
 package kafic;
 
-public class Racun {
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
+import kafic.sistemskeoperacije.SOIspisiStavkeListe;
+
+/**
+ * 
+ * Klasa koja predstavlja racun za narucene proizvode u kaficu.
+ * 
+ * @author Nikola Bakic
+ *
+ */
+
+public class Racun {
+	/**
+	 * String koji predstavlja naziv kafica.
+	 */
+	private final String nazivLokala = "Paradiso Cafe";
+	/**
+	 * Atribut koji predstavlja datum i vreme na koje se racun izdaje.
+	 */
+	private GregorianCalendar datum;
+	/**
+	 * Lista artikala koje je gost narucio.
+	 */
+	private LinkedList<Artikal> stavkeRacuna = new LinkedList<Artikal>();
+	/**
+	 * Objekat koji prestavlja radnika koji usluzuje odredjeni sto.
+	 */
+	private Radnik radnik;
+	/**
+	 * Integer koji predstavlja broj stola koji se opsluzuje.
+	 */
+	private int brojStola;
+	/**
+	 * Integer koji predstavlja potroseni iznos za odredjeni sto.
+	 */
+	private int zaUplatu;
+	/**
+	 * Integer koji prestavlja iznos koji gost uplatio.
+	 */
+	private int jeUplaceno;
+	/**
+	 * Integer koji predstavlja kusur.
+	 */
+	private int kusur = jeUplaceno - zaUplatu;
+	/**
+	 * String koji predstavlja sifru racuna.
+	 */
+	private String sifraRacuna;
+
+	/**
+	 * 
+	 * Metoda koja vraca naziv lokala.
+	 * 
+	 * @return nazivLokala
+	 */
+	public String getNazivLokala() {
+		return nazivLokala;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca vreme na koje se racun izdaje.
+	 * 
+	 * @return datum
+	 */
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+
+	/**
+	 *
+	 * Metoda kojom se postavlja datum racuna.
+	 * 
+	 * @param datum
+	 *            Predstavlja datum racuna.
+	 * 
+	 * @throws Exception
+	 *             Ako je:
+	 *             <ul>
+	 *             <li>Unet datum koji se odnosi na proslost</li>
+	 *             </ul>
+	 * 
+	 */
+	public void setDatum(GregorianCalendar datum) throws Exception {
+		if (datum.before(new GregorianCalendar())) {
+			throw new Exception("Racun ne moze biti izdat za prethodni period!");
+		}
+
+		this.datum = datum;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca listu svih narucenih artikala.
+	 * 
+	 * @return stavkeRacuna
+	 */
+	public LinkedList<Artikal> getStavkeRacuna() {
+		return stavkeRacuna;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca radnika koji izdaje racun.
+	 * 
+	 * @return radnik
+	 */
+	public Radnik getRadnik() {
+		return radnik;
+	}
+
+	/**
+	 * 
+	 * Metoda koja postavlja radnika za odredjeni sto.
+	 * 
+	 * @param radnik
+	 */
+	public void setRadnik(Radnik radnik) {
+		// TODO : Uraditi proveru za unos radnika
+		this.radnik = radnik;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca broj stola.
+	 * 
+	 * @return brojStola
+	 */
+	public int getBrojStola() {
+		return brojStola;
+	}
+
+	/**
+	 * 
+	 * Metoda koje postavlja broj stola na koji se racun odnosi.
+	 * 
+	 * @param brojStola
+	 *            Predstavlja broj stola.
+	 * 
+	 * @throws Exception
+	 *             Ako je:
+	 *             <ul>
+	 *             <li>Broj stola veci od 8.</li>
+	 *             </ul>
+	 */
+	public void setBrojStola(int brojStola) throws Exception {
+		if (brojStola > 8)
+			throw new Exception("Broj stolova je osam!");
+
+		this.brojStola = brojStola;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca iznos koji je potrosen za odredjeni sto.
+	 * 
+	 * @return zaUplatu
+	 */
+	public int getZaUplatu() {
+		return zaUplatu;
+	}
+
+	/**
+	 * 
+	 * Metoda koja postavlja iznos koji je potrosen za odredjeni sto.
+	 * 
+	 * @param zaUplatu
+	 *            Predstavlja potroseni iznos.
+	 * 
+	 * @throws Exception
+	 *             Ako je:
+	 *             <ul>
+	 *             <li>Iznos za uplatu manji od 0.</li>
+	 *             </ul>
+	 */
+	public void setZaUplatu(int zaUplatu) throws Exception {
+		if (zaUplatu < 0)
+			throw new Exception("Iznos ne moze biti negativan!");
+
+		this.zaUplatu = zaUplatu;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca iznos koji je uplacen za odredjeni sto.
+	 * 
+	 * @return jeUplaceno
+	 */
+	public int getJeUplaceno() {
+		return jeUplaceno;
+	}
+
+	/**
+	 * 
+	 * Metoda koja postavlja iznos koji je uplacen za odredjeni sto.
+	 * 
+	 * @param jeUplaceno
+	 *            Predstavlja uplacen iznos.
+	 * 
+	 * @throws Exception
+	 *             Ako je:
+	 *             <ul>
+	 *             <li>Uplaceni iznos manji od 0.</li>
+	 *             <li>Uplaceni iznos manji od potrosenog iznosa.</li>
+	 *             </ul>
+	 */
+	public void setJeUplaceno(int jeUplaceno) throws Exception {
+		if (jeUplaceno < 0)
+			throw new Exception("Iznos ne moze biti negativan!");
+		if (jeUplaceno < zaUplatu)
+			throw new Exception("Ne moze biti uplaceno manje od potrosenog iznosa!");
+
+		this.jeUplaceno = jeUplaceno;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca kusur za odredjeni sto.
+	 * 
+	 * @return kusur
+	 */
+	public int getKusur() {
+		return kusur;
+	}
+
+	/**
+	 * 
+	 * Metoda koja vraca sifru racuna.
+	 * 
+	 * @return sifraRacuna
+	 */
+	public String getSifraRacuna() {
+		return sifraRacuna;
+	}
+
+	/**
+	 * 
+	 * Metoda vraca string koji predstavlja racun, primer: 
+	 * 		"Paradiso Cafe"
+	 * 
+	 * Datum: 30.04.2018. 14:28 
+	 * Broj stola: 5 
+	 * Sifra racuna: 578941
+	 * 
+	 * Radnik: Marko Markovic
+	 * 
+	 * 
+	 * Coca-Cola 130 Domaca kafa 100
+	 * 
+	 * Za uplatu: 230 
+	 * Uplaceno je: 300 
+	 * Za povracaj: 70
+	 * 
+	 * Dodjite nam ponovo! Prijatan dan. :)
+	 * 
+	 * 
+	 * @return String koji prestavlja racun
+	 */
+	@Override
+	public String toString() {
+		return "\t\"" + nazivLokala + "\"\t\n\n" + "Datum: " + datum + "\nBroj stola: " + brojStola + "\nSifra racuna: "
+				+ sifraRacuna + "\n\nRadnik: " + radnik.getIme() + " " + radnik.getPrezime() + "\n\n"
+				+ SOIspisiStavkeListe.izvrsi(stavkeRacuna) + "\n\nZa uplatu: " + zaUplatu + "\nUplaceno je: "
+				+ jeUplaceno + "\nZa povracaj: " + kusur + "\nDodjite nam ponovo!" + "\nPrijatan dan. :)\n";
+	}
+
+	/**
+	 * Proverava da li su dva racuna ista, tj. da li su im sifre iste
+	 *
+	 * @param obj
+	 *            Referentni objekat za poredjenje
+	 *
+	 * @return
+	 *         <ul>
+	 *         <li><b>true</b> ako su racuni isti, tj. imaju iste sifre</li>
+	 *         <li><b>false</b> ako su racuni razliciti, tj. nemaju iste
+	 *         sifre</li>
+	 *         </ul>
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Racun other = (Racun) obj;
+		if (sifraRacuna == null) {
+			if (other.sifraRacuna != null)
+				return false;
+		} else if (!sifraRacuna.equals(other.sifraRacuna))
+			return false;
+		return true;
+	}
 }
---
+++ SistemZaKafic/src/kafic/sistemskeoperacije/SOIspisiStavkeListe.java
@@ -0,0 +1,28 @@
+package kafic.sistemskeoperacije;
+
+import java.util.LinkedList;
+
+import kafic.Artikal;
+
+/**
+ * 
+ * Klasa koja izvrsava operaciju ispisivanja svih stavki liste.
+ * 
+ * @author Nikola Bakic
+ *
+ */
+
+public class SOIspisiStavkeListe {
+
+	public static String izvrsi(LinkedList<Artikal> artikli) {
+		String stavke = null;
+		
+		for (int i = 0; i < artikli.size(); i++) {
+			Artikal a = artikli.get(i);
+
+			stavke += a.getNazivArtikla() + "\t\t" + a.getCenaArtikla() + "\n";
+		}
+		
+		return stavke;
+	}
+}
