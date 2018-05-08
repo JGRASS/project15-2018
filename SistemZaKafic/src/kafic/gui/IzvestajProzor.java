840739c9805e6ce8a614061ef059ad1c58e70db7
nikolab20 <nikola.bakic20@hotmail.com>
2018-05-08 15:39:42 +0200

    Dodata grafika na prozor izvestaj

--- SistemZaKafic/src/kafic/gui/IzvestajProzor.java
+++ SistemZaKafic/src/kafic/gui/IzvestajProzor.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Dimension;
+import java.awt.Font;
 
 public class IzvestajProzor extends JFrame {
 
 	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
+	private JPanel panel;
+	private JButton btnNewButton;
+	private JButton btnOdustani;
 
 	/**
 	 * Create the frame.
@@ -21,6 +31,46 @@ public class IzvestajProzor extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.add(getScrollPane(), BorderLayout.CENTER);
+		contentPane.add(getPanel(), BorderLayout.EAST);
 	}
-
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTextArea());
+		}
+		return scrollPane;
+	}
+	private JTextArea getTextArea() {
+		if (textArea == null) {
+			textArea = new JTextArea();
+		}
+		return textArea;
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setPreferredSize(new Dimension(120, 10));
+			panel.setLayout(null);
+			panel.add(getBtnNewButton());
+			panel.add(getBtnOdustani());
+		}
+		return panel;
+	}
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("Sacuvaj");
+			btnNewButton.setBounds(14, 5, 91, 29);
+			btnNewButton.setFont(new Font("Dialog", Font.PLAIN, 15));
+		}
+		return btnNewButton;
+	}
+	private JButton getBtnOdustani() {
+		if (btnOdustani == null) {
+			btnOdustani = new JButton("Odustani");
+			btnOdustani.setBounds(14, 211, 91, 29);
+			btnOdustani.setFont(new Font("Dialog", Font.PLAIN, 15));
+		}
+		return btnOdustani;
+	}
 }
